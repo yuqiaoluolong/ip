3eca3f35527e51ab9a6508b3d89179b85dde7329
yuqiaoluolong <yuqiaoluolong@gmail.com>
2020-08-31 21:27:25 +0800

    Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String Greet = "____________________________________________________________\n"
+                + " Hello! I'm Duke\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(Greet);
     }
 }
