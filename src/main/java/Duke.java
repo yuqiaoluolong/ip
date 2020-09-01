4879598521f6c5504022787c5b86117534352754
yuqiaoluolong <yuqiaoluolong@gmail.com>
2020-09-01 10:03:07 +0800

    Level-2: Add, List &customizes:  1. add the second indentation 			2. add a line between Duke's response and user's input command &optimizations:	1. delete unused import java.io.*;

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
-import java.io.*;
 
 public class Duke {
     public static String indentation = "    ";
+    public static String secondIndentation = "  ";
     public static String horizontalLine = indentation + "<------------------------------------------------------------>\n";
+    public static String[] Lists = new String[100];
+    public static int numberOfList = 0;
 
     public static void printStatement(String statement){
         System.out.println(horizontalLine + indentation + "Here is yuqiaoluolong's Duke: \n" + statement + horizontalLine);
@@ -18,7 +20,7 @@ public class Duke {
         String Greet = indentation + " Hello! I'm Duke\n"
                 + indentation + " What can I do for you?\n";
         String Bye = indentation + " Bye. Hope to see you again soon!\n";
-        String inputCommand = " ";
+        String inputCommand;
         boolean isBye = false;
 
         printStatement("Hello from\n" + logo);
@@ -27,12 +29,30 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         while(!isBye) {
             inputCommand = in.nextLine();
+            switch (inputCommand.trim()){
+                case "bye":
+                    printStatement(Bye);
+                    isBye = true;
+                    break;
+                case "list":
+                    System.out.print(horizontalLine);
+                    System.out.println(indentation + "Here is yuqiaoluolong's Duke: ");
+                    for(int i = 0; i < numberOfList; i++){
+                        System.out.println(indentation + secondIndentation + (i+1) + ". " + Lists[i]);
+                    }
+                    System.out.println(horizontalLine);
+                    break;
+                default:
+                    Lists[numberOfList] = inputCommand.trim();
+                    numberOfList++;
+                    printStatement(indentation + secondIndentation + "added: " + inputCommand + "\n");
+            }/*
             if(inputCommand.trim().equals("bye")){                  //must use .equal() instead of ==
                 printStatement(Bye);
                 isBye = true;
                 break;
             }
-            printStatement(indentation + " " + inputCommand + "\n");
+            printStatement(indentation + " added: " + inputCommand + "\n");*/
         }
 
     }
