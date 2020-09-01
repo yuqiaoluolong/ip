dc6fb157ad680e59d87852fd77b56438f8276be7
yuqiaoluolong <yuqiaoluolong@gmail.com>
2020-09-01 09:29:31 +0800

    Level-1: Greet, Echo, Exit & a little bit customizes: 1. add online of " Here is yuqiaoluolong's Duke: " before each echo. 					2. add a couple of "<" and ">" in the beginning and the end of the horizontalLine

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,39 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Duke {
+    public static String indentation = "    ";
+    public static String horizontalLine = indentation + "<------------------------------------------------------------>\n";
+
+    public static void printStatement(String statement){
+        System.out.println(horizontalLine + indentation + "Here is yuqiaoluolong's Duke: \n" + statement + horizontalLine);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String Greet = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println(Greet);
+        String Greet = indentation + " Hello! I'm Duke\n"
+                + indentation + " What can I do for you?\n";
+        String Bye = indentation + " Bye. Hope to see you again soon!\n";
+        String inputCommand = " ";
+        boolean isBye = false;
+
+        printStatement("Hello from\n" + logo);
+        printStatement(Greet);
+
+        Scanner in = new Scanner(System.in);
+        while(!isBye) {
+            inputCommand = in.nextLine();
+            if(inputCommand.trim().equals("bye")){                  //must use .equal() instead of ==
+                printStatement(Bye);
+                isBye = true;
+                break;
+            }
+            printStatement(indentation + " " + inputCommand + "\n");
+        }
+
     }
 }
