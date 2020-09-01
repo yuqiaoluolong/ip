833fd24256c92c207744768134d550fb616eb7c9
yuqiaoluolong <yuqiaoluolong@gmail.com>
2020-09-01 17:07:02 +0800

    Level 4. ToDos, Events, Deadlines & A-Inheritance original main classes: level-3 original Task classes: level-3
    To achieve level-4, need to apply polymorphism
    Task classes: add a method toString()
    Todo classes: extends Task
    Deadline classes: extends Task add one more variable by (ddl time)
    Event classes: extends Task add one more variable at (happening time)
    main class: edit the format of output according to level-4 requirements and add a condition when user typed unexpected strings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Duke {
     public static String indentation = "    ";
-    public static String secondIndentation = "  ";
-    public static String horizontalLine = indentation + "<------------------------------------------------------------>\n";
+    public static String doubleIndentation = indentation + "  ";
+    public static String tripleIndentation = doubleIndentation + "  ";
+    public static String horizontalLine = indentation
+            + "<------------------------------------------------------------>\n";
     public static Task[] Tasks = new Task[100];
     public static int numberOfTasks = 0;
 
     public static void printStatement(String statement){
-        System.out.println(horizontalLine + indentation + "Here is yuqiaoluolong's Duke: \n" + statement + horizontalLine);
+        System.out.println(horizontalLine + indentation + "Here is yuqiaoluolong's Duke: \n"
+                + statement + horizontalLine);
     }
 
     public static void main(String[] args) {
@@ -23,6 +26,7 @@ public class Duke {
         String inputCommand;
         boolean isBye = false;
         int doneNum;
+        String respond = " ";
 
         printStatement("Hello from\n" + logo);
         printStatement(greet);
@@ -33,10 +37,10 @@ public class Duke {
             switch (inputCommand.trim()) {
             case "list":
                 System.out.print(horizontalLine);
-                System.out.println(indentation + "Here is yuqiaoluolong's Duke: ");
+                System.out.println(indentation + "Here is yuqiaoluolong's Duke: \n" +
+                        "      Here are the tasks in your list:");
                 for(int i = 0; i < numberOfTasks; i++){
-                    System.out.println(indentation + secondIndentation + (i+1) + ".["
-                            + Tasks[i].getStatusIcon() + "] " + Tasks[i].description);
+                    System.out.println(doubleIndentation + (i+1) + "." + Tasks[i].toString());
                 }
                 System.out.println(horizontalLine);
                 break;
@@ -45,17 +49,37 @@ public class Duke {
                 isBye = true;
                 break;
             default:
-                if(inputCommand.contains("done")){
+                if (inputCommand.contains("done")) {
                     doneNum = Integer.parseInt(inputCommand.replace("done", " ").trim());
                     Tasks[doneNum-1].markAsDone();
-                    printStatement(indentation + secondIndentation + "Nice! I've marked this task as done: \n"
-                            + indentation + secondIndentation + secondIndentation + " ["
-                            + Tasks[doneNum-1].getStatusIcon() + "] " + Tasks[doneNum-1].description + "\n");
-                    break;
+                    respond = doubleIndentation + "Nice! I've marked this task as done: \n"
+                            + tripleIndentation + Tasks[doneNum-1].toString() + "\n";
+                } else if (inputCommand.contains("todo")||inputCommand.contains("deadline")
+                        ||inputCommand.contains("event")){
+                    if (inputCommand.contains("todo")) {
+                        Tasks[numberOfTasks] = new Todo(inputCommand.replace("todo"," ").trim());
+                    } else if (inputCommand.contains("deadline")) {
+                        Tasks[numberOfTasks] = new Deadline(inputCommand.substring(inputCommand.indexOf("deadline")+8,
+                                inputCommand.indexOf("/")).trim(),
+                                inputCommand.substring(inputCommand.indexOf("/by")+3).trim());
+
+                    } else if (inputCommand.contains("event")) {
+                        Tasks[numberOfTasks] = new Event(inputCommand.substring(inputCommand.indexOf("event")+5,
+                                inputCommand.indexOf("/")).trim(),
+                                inputCommand.substring(inputCommand.indexOf("/at")+3).trim());
+                    }
+                    respond = doubleIndentation + "Got it. I've added this task:\n"
+                            + tripleIndentation + Tasks[numberOfTasks].toString() + "\n" + doubleIndentation
+                            + "Now you have " + (numberOfTasks+1) + " tasks in the list.\n";
+                    numberOfTasks++;
+                } else {
+                    respond = doubleIndentation + "Sorry, I don't know what's your command. Can you type again?\n";
                 }
+                printStatement(respond);
+                /*
                 Tasks[numberOfTasks] = new Task(inputCommand.trim());
                 numberOfTasks++;
-                printStatement(indentation + secondIndentation + "added: " + inputCommand + "\n");
+                printStatement(doubleIndentation + "added: " + inputCommand + "\n");*/
             }
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -14,4 +14,8 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
+
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
 }
