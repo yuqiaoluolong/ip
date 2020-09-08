8ad02295f7c1e5057c08734cdbd3dbbffb9a2709
yuqiaoluolong <yuqiaoluolong@gmail.com>
2020-09-08 23:25:57 +0800

    CodeQuality
    correct some names of constants
    add some methods to make the code inside main class clearer

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,78 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static String indentation = "    ";
-    public static String doubleIndentation = indentation + "  ";
-    public static String tripleIndentation = doubleIndentation + "  ";
-    public static String horizontalLine = indentation
+    public static final int MAX_NUM_TASKS = 100;
+    public static final String INDENTATION = "    ";
+    public static final String DOUBLEINDENTATION = INDENTATION + "  ";
+    public static final String TRIPLEINDENTATION = DOUBLEINDENTATION + "  ";
+    public static final String HORIZONTALLINE = INDENTATION
             + "<------------------------------------------------------------>\n";
-    public static Task[] Tasks = new Task[100];
+    public static Task[] tasks = new Task[MAX_NUM_TASKS];
     public static int numberOfTasks = 0;
 
-    public static void printStatement(String statement){
-        System.out.println(horizontalLine + indentation + "Here is yuqiaoluolong's Duke: \n"
-                + statement + horizontalLine);
+    public static void printStatement(String statement) {
+        System.out.println(HORIZONTALLINE + INDENTATION + "Here is yuqiaoluolong's Duke: \n"
+                + statement + HORIZONTALLINE);
+    }
+    public static int getDoneNum(String inputCommand) {
+        return Integer.parseInt(inputCommand.replace("done", " ").trim());
+    }
+    public static String getDescriptiong(String index, String inputCommand) {
+        if(index == "deadline") {
+            return inputCommand.substring(inputCommand.indexOf("deadline")+8, inputCommand.indexOf("/")).trim();
+        } else {
+            return inputCommand.substring(inputCommand.indexOf("event")+5, inputCommand.indexOf("/")).trim();
+        }
+    }
+    public static void executeListCommand(String inputcommanc) {
+        System.out.print(HORIZONTALLINE);
+        System.out.println(INDENTATION + "Here is yuqiaoluolong's Duke: \n" +
+                "      Here are the tasks in your list:");
+        for(int i = 0; i < numberOfTasks; i++){
+            System.out.println(DOUBLEINDENTATION + (i+1) + "." + tasks[i].toString());
+        }
+        System.out.println(HORIZONTALLINE);
+    }
+    public static void executeDoneCommand(String inputCommand) {
+        int doneNum = getDoneNum(inputCommand);
+        tasks[doneNum-1].markAsDone();
+        printStatement(DOUBLEINDENTATION + "Nice! I've marked this task as done: \n"
+                + TRIPLEINDENTATION + tasks[doneNum-1].toString() + "\n");
+    }
+    public static void executeTodoCommand(Task[] tasks, int numberOfTasks, String inputCommand) {
+        tasks[numberOfTasks] = new Todo(inputCommand.replace("todo"," ").trim());
+        printStatement(DOUBLEINDENTATION + "Got it. I've added this task:\n"
+                + TRIPLEINDENTATION + tasks[numberOfTasks].toString() + "\n" + DOUBLEINDENTATION
+                + "Now you have " + (numberOfTasks+1) + " tasks in the list.\n");
+    }
+    public static void executeDeadlineCommand(Task[] tasks, int numberOfTasks, String inputCommand) {
+        String description = getDescriptiong("deadline", inputCommand);
+        String date = inputCommand.substring(inputCommand.indexOf("/by")+3).trim();
+        tasks[numberOfTasks] = new Deadline(description, date);
+        printStatement(DOUBLEINDENTATION + "Got it. I've added this task:\n"
+                + TRIPLEINDENTATION + tasks[numberOfTasks].toString() + "\n" + DOUBLEINDENTATION
+                + "Now you have " + (numberOfTasks+1) + " tasks in the list.\n");
+    }
+    public static void executeEventCommand(Task[] tasks, int numberOfTasks, String inputCommand) {
+        String description = getDescriptiong("event", inputCommand);
+        String date = inputCommand.substring(inputCommand.indexOf("/at")+3).trim();
+        tasks[numberOfTasks] = new Event(description, date);
+        printStatement(DOUBLEINDENTATION + "Got it. I've added this task:\n"
+                + TRIPLEINDENTATION + tasks[numberOfTasks].toString() + "\n" + DOUBLEINDENTATION
+                + "Now you have " + (numberOfTasks+1) + " tasks in the list.\n");
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        final String LOGO = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String greet = indentation + " Hello! I'm Duke\n"
-                + indentation + " What can I do for you?\n";
-        String bye = indentation + " Bye. Hope to see you again soon!\n";
-        String wrongWarning = "Sorry, I don't know what's your command. Can you type again?\n";
+        final String GREET = INDENTATION + " Hello! I'm Duke\n"
+                + INDENTATION + " What can I do for you?\n";
+        final String BYE = INDENTATION + " Bye. Hope to see you again soon!\n";
+        final String WRONGMESSAGE = "Sorry, I don't know what's your command. Can you type again?\n";
+        boolean isNewTask;
         String inputCommand;
         boolean isBye = false;
         int doneNum;
@@ -31,60 +80,38 @@ public class Duke {
         String description;
         String date;
 
-        printStatement("Hello from\n" + logo);          // greet in the beginning
-        printStatement(greet);
+        printStatement("Hello from\n" + LOGO);          // greet in the beginning
+        printStatement(GREET);
 
         Scanner in = new Scanner(System.in);
         while (!isBye) {
             inputCommand = in.nextLine();
+            isNewTask = inputCommand.contains("todo") || inputCommand.contains("deadline")
+                    || inputCommand.contains("event");
             switch (inputCommand.trim()) {
             case "list":
-                System.out.print(horizontalLine);
-                System.out.println(indentation + "Here is yuqiaoluolong's Duke: \n" +
-                        "      Here are the tasks in your list:");
-                for(int i = 0; i < numberOfTasks; i++){
-                    System.out.println(doubleIndentation + (i+1) + "." + Tasks[i].toString());
-                }
-                System.out.println(horizontalLine);
+                executeListCommand(inputCommand);
                 break;
             case "bye":
-                printStatement(bye);
+                printStatement(BYE);
                 isBye = true;
                 break;
             default:
                 if (inputCommand.contains("done")) {
-                    doneNum = Integer.parseInt(inputCommand.replace("done", " ").trim());
-                    Tasks[doneNum-1].markAsDone();
-                    outputStatement = doubleIndentation + "Nice! I've marked this task as done: \n"
-                            + tripleIndentation + Tasks[doneNum-1].toString() + "\n";
-                } else if (inputCommand.contains("todo")||inputCommand.contains("deadline")
-                        ||inputCommand.contains("event")){
+                    executeDoneCommand(inputCommand);
+                } else if (isNewTask) {
                     if (inputCommand.contains("todo")) {
-                        Tasks[numberOfTasks] = new Todo(inputCommand.replace("todo"," ").trim());
+                        executeTodoCommand(tasks, numberOfTasks, inputCommand);
                     } else if (inputCommand.contains("deadline")) {
-                        description = inputCommand.substring(inputCommand.indexOf("deadline")+8,
-                                inputCommand.indexOf("/")).trim();
-                        date = inputCommand.substring(inputCommand.indexOf("/by")+3).trim();
-                        Tasks[numberOfTasks] = new Deadline(description, date);
+                        executeDeadlineCommand(tasks, numberOfTasks, inputCommand);
 
                     } else if (inputCommand.contains("event")) {
-                        description = inputCommand.substring(inputCommand.indexOf("event")+5,
-                                inputCommand.indexOf("/")).trim();
-                        date = inputCommand.substring(inputCommand.indexOf("/at")+3).trim();
-                        Tasks[numberOfTasks] = new Event(description, date);
+                        executeEventCommand(tasks, numberOfTasks, inputCommand);
                     }
-                    outputStatement = doubleIndentation + "Got it. I've added this task:\n"
-                            + tripleIndentation + Tasks[numberOfTasks].toString() + "\n" + doubleIndentation
-                            + "Now you have " + (numberOfTasks+1) + " tasks in the list.\n";
                     numberOfTasks++;
                 } else {
-                    outputStatement = doubleIndentation + wrongWarning;
+                    printStatement(DOUBLEINDENTATION + WRONGMESSAGE);
                 }
-                printStatement(outputStatement);
-                /*
-                Tasks[numberOfTasks] = new Task(inputCommand.trim());
-                numberOfTasks++;
-                printStatement(doubleIndentation + "added: " + inputCommand + "\n");*/
             }
         }
 
